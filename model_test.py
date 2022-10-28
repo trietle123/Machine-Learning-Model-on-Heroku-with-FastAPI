45627a15b901859ab8647921578b37a6b8331026
LAPTOP-AKLISRM2\HCuser <triet.leminh.de@gmail.com>
2022-10-28 14:25:58 +0200

    Move to parent folder to fix Action workflow

--- model_test.py
+++ model_test.py
@@ -9,7 +9,7 @@ from starter.ml.data import process_data
 from starter.ml.model import train_model, compute_model_metrics, inference
 
 # Add code to load in the data.
-data = pd.read_csv("starter/data/census_clean.csv")
+data = pd.read_csv("data/census_clean.csv")
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
